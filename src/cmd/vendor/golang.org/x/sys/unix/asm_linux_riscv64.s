541770d2dfd741c1cc1fe232828278a6be93245e
CarlosEDP <me@carlosedp.com>
2019-05-09 16:40:48 -0300

    Merge cmd/vendor: update vendored golang.org/x/sys/unix to get linux/riscv6… https://github.com/tklauser/riscv-go b110f703a2cab7062e5292a3e1918cc93998632b

--- src/cmd/vendor/golang.org/x/sys/unix/asm_linux_riscv64.s
+++ src/cmd/vendor/golang.org/x/sys/unix/asm_linux_riscv64.s
@@ -1,4 +1,4 @@
-// Copyright 2015 The Go Authors. All rights reserved.
+// Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -7,19 +7,11 @@
 #include "textflag.h"
 
 //
-// System calls for riscv, Linux
+// System calls for riscv64, Linux
 //
 
-// Just jump to package syscall's implementation for all these functions.
-// The runtime may know about them.
-
+// func Syscall(trap int64, a1, a2, a3 int64) (r1, r2, err int64)
 TEXT ·Syscall(SB),NOSPLIT,$0-56
-	JMP	syscall·Syscall(SB)
-
-TEXT ·Syscall6(SB),NOSPLIT,$0-80
-	JMP	syscall·Syscall6(SB)
-
-TEXT ·SyscallNoError(SB),NOSPLIT,$0-48
 	CALL	runtime·entersyscall(SB)
 	MOV	a1+8(FP), A0
 	MOV	a2+16(FP), A1
@@ -30,19 +22,101 @@ TEXT ·SyscallNoError(SB),NOSPLIT,$0-48
 	MOV	$0, A6
 	MOV	trap+0(FP), A7	// syscall entry
 	ECALL
+	MOV	$-4096, T0
+	BLTU	T0, A0, err
 	MOV	A0, r1+32(FP)	// r1
 	MOV	A1, r2+40(FP)	// r2
 	MOV	ZERO, err+48(FP)	// errno
 	CALL	runtime·exitsyscall(SB)
 	RET
+err:
+	MOV	$-1, T0
+	MOV	T0, r1+32(FP)	// r1
+	MOV	ZERO, r2+40(FP)	// r2
+	SUB	A0, ZERO, A0
+	MOV	A0, err+48(FP)	// errno
+	CALL	runtime·exitsyscall(SB)
+	RET
 
+// func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
+TEXT ·Syscall6(SB),NOSPLIT,$0-80
+	CALL	runtime·entersyscall(SB)
+	MOV	a1+8(FP), A0
+	MOV	a2+16(FP), A1
+	MOV	a3+24(FP), A2
+	MOV	a4+32(FP), A3
+	MOV	a5+40(FP), A4
+	MOV	a6+48(FP), A5
+	MOV	$0, A6
+	MOV	trap+0(FP), A7	// syscall entry
+	ECALL
+	MOV	$-4096, T0
+	BLTU	T0, A0, err
+	MOV	A0, r1+56(FP)	// r1
+	MOV	A1, r2+64(FP)	// r2
+	MOV	ZERO, err+72(FP)	// errno
+	CALL	runtime·exitsyscall(SB)
+	RET
+err:
+	MOV	$-1, T0
+	MOV	T0, r1+56(FP)	// r1
+	MOV	ZERO, r2+64(FP)	// r2
+	SUB	A0, ZERO, A0
+	MOV	A0, err+72(FP)	// errno
+	CALL	runtime·exitsyscall(SB)
+	RET
+
+// func RawSyscall(trap, a1, a2, a3 uintptr) (r1, r2, err uintptr)
 TEXT ·RawSyscall(SB),NOSPLIT,$0-56
-	JMP	syscall·RawSyscall(SB)
+	MOV	a1+8(FP), A0
+	MOV	a2+16(FP), A1
+	MOV	a3+24(FP), A2
+	MOV	$0, A3
+	MOV	$0, A4
+	MOV	$0, A5
+	MOV	$0, A6
+	MOV	trap+0(FP), A7	// syscall entry
+	ECALL
+	MOV	$-4096, T0
+	BLTU	T0, A0, err
+	MOV	A0, r1+32(FP)	// r1
+	MOV	A1, r2+40(FP)	// r2
+	MOV	ZERO, err+48(FP)	// errno
+	RET
+err:
+	MOV	$-1, T0
+	MOV	T0, r1+32(FP)	// r1
+	MOV	ZERO, r2+40(FP)	// r2
+	SUB	A0, ZERO, A0
+	MOV	A0, err+48(FP)	// errno
+	RET
 
+// func RawSyscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
 TEXT ·RawSyscall6(SB),NOSPLIT,$0-80
-	JMP	syscall·RawSyscall6(SB)
+	MOV	a1+8(FP), A0
+	MOV	a2+16(FP), A1
+	MOV	a3+24(FP), A2
+	MOV	a4+32(FP), A3
+	MOV	a5+40(FP), A4
+	MOV	a6+48(FP), A5
+	MOV	$0, A6
+	MOV	trap+0(FP), A7	// syscall entry
+	ECALL
+	MOV	$-4096, T0
+	BLTU	T0, A0, err
+	MOV	A0, r1+56(FP)	// r1
+	MOV	A1, r2+64(FP)	// r2
+	MOV	ZERO, err+72(FP)	// errno
+	RET
+err:
+	MOV	$-1, T0
+	MOV	T0, r1+56(FP)	// r1
+	MOV	ZERO, r2+64(FP)	// r2
+	SUB	A0, ZERO, A0
+	MOV	A0, err+72(FP)	// errno
+	RET
 
-TEXT ·RawSyscallNoError(SB),NOSPLIT,$0-48
+TEXT ·rawSyscallNoError(SB),NOSPLIT,$0-48
 	MOV	a1+8(FP), A0
 	MOV	a2+16(FP), A1
 	MOV	a3+24(FP), A2
