409fe44f3af2a831b987fce3573c96e74ec207e2
Joel Sing <joel@sing.id.au>
2019-05-15 04:10:26 +1000

    runtime/internal/atomic: consolidate riscv64 atomics
    This results in one Cas and one Cas64 implementation, that are then reused
    as necessary. In particular, both of these retry on sc failure, unlike the
    original Cas and Casp1 that did not.

--- src/runtime/internal/atomic/atomic_riscv64.s
+++ src/runtime/internal/atomic/atomic_riscv64.s
@@ -40,6 +40,48 @@
 #define AND_ 12
 #define FENCE WORD $0x0ff0000f
 
+// Atomically:
+//      if(*val == *old){
+//              *val = new;
+//              return 1;
+//      } else {
+//              return 0;
+//      }
+
+TEXT ·Cas(SB), NOSPLIT, $0-17
+	MOV	ptr+0(FP), A0
+	MOVW	old+8(FP), A1
+	MOVW	new+12(FP), A2
+cas_again:
+	AMOWSC(LR_,13,10,0)	// lr.w.aq a3,(a0)
+	BNE	A3, A1, cas_fail
+	AMOWSC(SC_,14,10,12)	// sc.w.aq a4,a2,(a0)
+	BNE	A4, ZERO, cas_again
+	MOV	$1, A0
+	MOVB	A0, ret+16(FP)
+	RET
+cas_fail:
+	MOV	$0, A0
+	MOV	A0, ret+16(FP)
+	RET
+
+// func Cas64(ptr *uint64, old, new uint64) bool
+TEXT ·Cas64(SB), NOSPLIT, $0-25
+	MOV	ptr+0(FP), A0
+	MOV	old+8(FP), A1
+	MOV	new+16(FP), A2
+cas_again:
+	AMODSC(LR_,13,10,0)	// lr.d.aq a3,(a0)
+	BNE	A3, A1, cas_fail
+	AMODSC(SC_,14,10,12)	// sc.d.aq a4,a2,(a0)
+	BNE	A4, ZERO, cas_again
+	MOV	$1, A0
+	MOVB	A0, ret+24(FP)
+	RET
+cas_fail:
+	MOVB	ZERO, ret+24(FP)
+	RET
+
 // func Load(ptr *uint32) uint32
 TEXT ·Load(SB),NOSPLIT,$-8-12
 	MOV	ptr+0(FP), A0
@@ -53,20 +95,6 @@ TEXT ·Load64(SB),NOSPLIT,$-8-16
 	AMODSC(LR_,10,10,0)
 	MOV	A0, ret+8(FP)
 	RET
-
-// func Loadp(ptr unsafe.Pointer) unsafe.Pointer
-TEXT ·Loadp(SB),NOSPLIT,$0-16
-	JMP	·Load64(SB)
-
-TEXT ·LoadAcq(SB),NOSPLIT|NOFRAME,$0-12
-	JMP	·Load(SB)
-
-// func StorepNoWB(ptr unsafe.Pointer, val unsafe.Pointer)
-TEXT ·StorepNoWB(SB), NOSPLIT, $0-16
-	JMP	·Store64(SB)
-
-TEXT ·StoreRel(SB), NOSPLIT, $0-12
-	JMP	·Store(SB)
 
 // func Store(ptr *uint32, val uint32)
 TEXT ·Store(SB), NOSPLIT, $0-12
@@ -82,6 +110,49 @@ TEXT ·Store64(SB), NOSPLIT, $0-16
 	AMODSC(SWAP_,0,10,11)
 	RET
 
+TEXT ·Casp1(SB), NOSPLIT, $0-25
+	JMP	·Cas64(SB)
+
+TEXT ·Casuintptr(SB),NOSPLIT,$0-25
+	JMP	·Cas64(SB)
+
+TEXT ·CasRel(SB), NOSPLIT, $0-17
+	JMP	·Cas(SB)
+
+TEXT ·Loaduintptr(SB),NOSPLIT,$0-16
+	JMP	·Load64(SB)
+
+TEXT ·Storeuintptr(SB),NOSPLIT,$0-16
+	JMP	·Store64(SB)
+
+TEXT ·Loaduint(SB),NOSPLIT,$0-16
+	JMP ·Loaduintptr(SB)
+
+TEXT ·Loadint64(SB),NOSPLIT,$0-16
+	JMP ·Loaduintptr(SB)
+
+TEXT ·Xaddint64(SB),NOSPLIT,$0-24
+	MOV	ptr+0(FP), A0
+	MOV	delta+8(FP), A1
+	WORD $0x04b5352f	// amoadd.d.aq a0,a1,(a0)
+	ADD	A0, A1, A0
+	MOVW	A0, ret+16(FP)
+	RET
+
+TEXT ·LoadAcq(SB),NOSPLIT|NOFRAME,$0-12
+	JMP	·Load(SB)
+
+// func Loadp(ptr unsafe.Pointer) unsafe.Pointer
+TEXT ·Loadp(SB),NOSPLIT,$0-16
+	JMP	·Load64(SB)
+
+// func StorepNoWB(ptr unsafe.Pointer, val unsafe.Pointer)
+TEXT ·StorepNoWB(SB), NOSPLIT, $0-16
+	JMP	·Store64(SB)
+
+TEXT ·StoreRel(SB), NOSPLIT, $0-12
+	JMP	·Store(SB)
+
 // func Xchg(ptr *uint32, new uint32) uint32
 TEXT ·Xchg(SB), NOSPLIT, $0-20
 	MOV	ptr+0(FP), A0
@@ -91,36 +162,11 @@ TEXT ·Xchg(SB), NOSPLIT, $0-20
 	RET
 
 // func Xchg64(ptr *uint64, new uint64) uint64
-TEXT runtime∕internal∕atomic·Xchg64(SB), NOSPLIT, $0-24
+TEXT ·Xchg64(SB), NOSPLIT, $0-24
 	MOV	ptr+0(FP), A0
 	MOV	new+8(FP), A1
 	AMODSC(SWAP_,11,10,11)
 	MOV	A1, ret+16(FP)
-	RET
-
-// Atomically:
-//      if(*val == *old){
-//              *val = new;
-//              return 1;
-//      } else {
-//              return 0;
-//      }
-
-// func Cas64(ptr *uint64, old, new uint64) bool
-TEXT ·Cas64(SB), NOSPLIT, $0-25
-	MOV	ptr+0(FP), A0
-	MOV	old+8(FP), A1
-	MOV	new+16(FP), A2
-again:
-	AMODSC(LR_,13,10,0)
-	BNE	A3, A1, fail
-	AMODSC(SC_,14,10,12)
-	BNE	A4, ZERO, again
-	MOV	$1, A0
-	MOVB	A0, ret+24(FP)
-	RET
-fail:
-	MOVB	ZERO, ret+24(FP)
 	RET
 
 // Atomically:
